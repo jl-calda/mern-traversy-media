8ce70b8b047cf871388856879203151e4cd9e69d
JLCalda <jlcaldadev23@gmail.com>
2023-02-21 12:54:21 +0800

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("test");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const router = require("./routes/goalRoutes");
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+router.get(`/api/goals`, router);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
