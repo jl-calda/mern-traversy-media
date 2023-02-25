679709b86d6fe3118b59903eea7450dbdfabb23a
JLCalda <jlcaldadev23@gmail.com>
2023-02-25 18:15:30 +0800

    update cors

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
 
-app.use(cors());
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
