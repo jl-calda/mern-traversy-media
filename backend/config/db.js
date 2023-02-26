e571c9f8f80a7815bb37a94fbb441e794572b6a6
JLCalda <jlcaldadev23@gmail.com>
2023-02-26 12:47:52 +0800

    added some options on mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
